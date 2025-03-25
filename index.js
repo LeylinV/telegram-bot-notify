c6e3b7d79e84a9cd1731833389dd8a6d381c1d2c
LeylinV <d.rakipov@alef.im>
2025-03-25 05:18:31 +0500

    feat: логирование токена

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const path = require('path');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!token) {
+    console.error('Ошибка: Токен бота не найден в переменных окружения!');
+    console.log('Текущие переменные:', process.env); // Для дебага
+    process.exit(1);
+  }
+  console.log('Бот запускается с токеном:', token.slice(0, 5) + '...'); // Логируем часть токена для проверки
+
 const bot = new TelegramBot(token, { polling: true, request: {
     agentOptions: {
         keepAlive: true,
